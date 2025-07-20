f6ed2ae95c433ea2a8a5584085b3d40e97414fda
JESSE MWENDWA NDUNDA <jessendunda2@gmail.com>
2025-07-20 21:56:43 +0300

    add cors and backend files

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import jsonServer from "json-server";
+import cors from "cors";
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+server.use(cors()); 
 server.use(middlewares);
 server.use(router);
 
